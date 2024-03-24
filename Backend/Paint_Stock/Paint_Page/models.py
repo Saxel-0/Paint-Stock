ffdbfc3a0be74dccbe0a658b8a2dee74be58bbad
Saxel-0 <59002917+Saxel-0@users.noreply.github.com>
2024-03-23 20:57:33 -0700

    Created models for paints and users, including a user manager that allows for both the created and deletion of users

--- Backend/Paint_Stock/Paint_Page/models.py
+++ Backend/Paint_Stock/Paint_Page/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Paints(models.Model):
+    colour = models.CharField(max_length=50)
+    stock_level = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.colour
+    
+class SiteUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+                raise ValueError(_('A Username is required'))
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def delete_user(self, user):
+         user.delete()
+         
+class SiteUser(AbstractUser):
+    username = models.CharField(unique=True,max_length=30)
+    is_admin = models.BooleanField(default=False)
+    is_editor = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = SiteUserManager()
+
+    def __str__(self):
+        return self.username
+    
--- Backend/Paint_Stock/Paint_Page/views.py
+++ Backend/Paint_Stock/Paint_Page/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import SiteUser, Paints
 
-# Create your views here.
+
+
